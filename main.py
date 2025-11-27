d7c5fb515063dbb8cc0498491fb7e5c2e07d2d09
Duane Edmonds <duane.edmonds@gmail.com>
2025-11-27 09:33:47 -0800

    updated completion message. added rich console.

--- main.py
+++ main.py
@@ -1,30 +1,79 @@
 #!/home/dedmonds/repos/do/.venv/bin/python
 import sys
 from time import sleep
+from rich.console import Console
+from random import randint
 from playsound3 import playsound
 
-SOUND_PATH = '/home/dedmonds/repos/do/breathe.mp3'
+
+SOUND_PATH: str = '/home/dedmonds/repos/do/breathe.mp3'
 
 
-def main():
+# console colors
+CDIM: str = '#616161'
+CEMPH: str = '#faf6e4'
+CNORM: str = 'default'
+CERR: str = '#ff0000'
+CWARN: str = '#fff670'
+
+
+console = Console()
+
+
+messages: tuple[str, ...] = (
+    'return to yourself',
+    'let go',
+    'breathe',
+    'continue',
+    'be one with the river',
+    'attention softens, clarity returns',
+    'what you tended now tends you',
+    'carry no burden',
+    'love',
+    'empty your hands',
+    'be without being',
+    'let the moment rest',
+    'soften with each breath',
+    'let the moment occur',
+    'the way opens as you do',
+    'allow the next step to take you',
+    'listen to your silence',
+    'lean into stillness',
+    'hold nothing',
+    'what is is enough',
+    'be gentle',
+    'it starts now',
+    'pause',
+    'empty your step',
+    'be the breeze',
+    'be what is',
+    'open',
+    'fear is the illusion',
+)
+
+
+def main() -> None:
     if not sys.argv[1:]:
-        print("error: missing argument(s)")
+        console.print(f"  [{CERR}]error[/{CERR}]: missing argument(s)")
         return
 
     try:
         focus_min: float = float(sys.argv[1]) * 60
     except ValueError:
-        print("error: bad input")
+        console.print(f"  [{CERR}]error[/{CERR}]: bad input")
         return
 
     if sys.argv[2:]:
-        print(f"  focus: {sys.argv[2]}")
+        focus_note: str = ' '.join(sys.argv[2:])
+        console.print(f"  [{CDIM}]focus:[/{CDIM}] [{CEMPH}]{focus_note}[/{CEMPH}]")
 
     sleep(focus_min)
 
     playsound(SOUND_PATH)
 
-    print(f"  success: enjoy your day")
+    message: str = messages[randint(0, len(messages) - 1)]
+
+    console.print(f"  [{CDIM}]done:[/{CDIM}] \"[{CNORM}]{message}[/{CNORM}]\"")
 
 
 if __name__ == "__main__":
